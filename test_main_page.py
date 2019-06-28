c09b93a875a47a36f45a4f4276093dc2a20c03ba
d.ivaka <ivakus@mail.ru>
2019-06-28 15:37:04 +0300

    Модуль 4 глава 4.2 выолнены задания до пункта 4

---
+++ test_main_page.py
@@ -0,0 +1,9 @@
+from .pages.login_page import LoginPage
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=midsummer"
+    page = LoginPage(browser,link)
+    page.open()
+    page.should_be_login_page()
+    #page.go_to_login_page()
