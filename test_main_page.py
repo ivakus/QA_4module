010a76761bf6b724d55f87b763d444815501e1f8
d.ivaka <ivakus@mail.ru>
2019-06-28 15:40:13 +0300

    Модуль 4 глава 4.2 реализован класс заглушка LoginPage, функции класса пока заглушки

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,7 @@
 from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = "http://selenium1py.pythonanywhere.com/accounts/login"
     link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=midsummer"
     page = LoginPage(browser,link)
     page.open()
