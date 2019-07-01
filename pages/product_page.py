582203ebbd789a647e8c912f7bdd217a7f93d416
d.ivaka <ivakus@mail.ru>
2019-07-01 16:10:34 +0300

    Модуль 4 глава 4.3 решена 3 задача

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,18 +1,18 @@
-import math
+
 from .base_page import BasePage
 from .locators import ProductPageLocator
-#from .login_page import LoginPage
+
 
 class ProductPage(BasePage):
     def add_product_to_cart_(self):
+        self.book_name = self.get_book_name()
+
+
         self.should_be_add_to_cart_button()
-        self.should_be_book_name()
-        self.book_name = self.browser.find_element(*ProductPageLocator.BOOK_NAME).text
-        print (self.book_name)
         link = self.browser.find_element(*ProductPageLocator.ADD_CART_BOTTUN)
         link.click()
         self.solve_quiz_and_get_code()
-
+        self.get_in_cart_book_name()
 
     def should_be_add_to_cart_button(self):
         assert self.is_element_present(*ProductPageLocator.ADD_CART_BOTTUN), "ADD Button  is not presented"
@@ -20,18 +20,9 @@ class ProductPage(BasePage):
     def should_be_book_name(self):
         assert self.is_element_present(*ProductPageLocator.BOOK_NAME), "Book name  is not presented"
 
-    def solve_quiz_and_get_code(self):
-        alert = self.browser.switch_to.alert
-        x = alert.text.split(" ")[2]
-        answer = str(math.log(abs((12 * math.sin(float(x))))))
-        alert.send_keys(answer)
-        alert.accept()
-        try:
-            alert = self.browser.switch_to.alert
-            print("Your code: {}".format(alert.text))
-            alert.accept()
-        except NoAlertPresentException:
-            print("No second alert presented")
+    def get_book_name (self):
+        self.should_be_book_name()
+        return self.browser.find_element(*ProductPageLocator.BOOK_NAME).text
+
     def get_in_cart_book_name(self):
-        assert  self.book_name in self.browser.find_element(*ProductPageLocator.BOOK_NAME_IN_CART).text, "Book name in cart not equal  book name in add page  {}".format(
-            self.book_name)
+        assert  self.book_name == self.browser.find_element(*ProductPageLocator.BOOK_NAME_IN_CART).text, "Book name in cart not equal  book name in add page  {}".format(self.book_name)
--- test_guest_can_add_product_to_cart.py
+++ test_guest_can_add_product_to_cart.py
@@ -1,9 +1,22 @@
 import time
+import pytest
 from .pages.product_page import ProductPage
-def test_guest_can_add_product_to_cart(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_cart(browser,link):
+    #link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
     page.open()
     #time.sleep(5)
     page.add_product_to_cart_()
+    #time.sleep(1000)
